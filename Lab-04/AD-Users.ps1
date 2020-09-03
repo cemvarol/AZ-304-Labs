d486c1237555f0dc3d9c60026a0a54f76be5e3ce
cemvarol <cem.varol@outlook.com>
2020-09-03 16:17:34 +0100

    Update AD-Users.ps1

--- Lab-04/AD-Users.ps1
+++ Lab-04/AD-Users.ps1
@@ -2,10 +2,10 @@ param(
 [int]$Users=(read-host " How many Users to create ?"),
 [string] $domainname= [System.DirectoryServices.ActiveDirectory.Domain]::GetCurrentDomain().Name
 )
-#Create Groups OU and 'Los Potatos' Group in Groups OU
+#Create Groups OU and 'Los AdUsers' Group in Groups OU
 New-ADOrganizationalUnit Groups -ProtectedFromAccidentalDeletion $false
 $ADOU=Get-ADOrganizationalUnit -Filter "name -eq 'groups'"
-new-adgroup "Los Potatos" -GroupScope Global -Path $AdOU.DistinguishedName 
+new-adgroup "Los AdUsers" -GroupScope Global -Path $AdOU.DistinguishedName 
 #Create the rest of the OU's
 New-ADOrganizationalUnit 0-Zeros -ProtectedFromAccidentalDeletion $false
 New-ADOrganizationalUnit 1-Tens -ProtectedFromAccidentalDeletion $false
@@ -20,42 +20,42 @@ foreach ($i in 1..$Users) {
     }else {
         $i = $i
     }
-    New-ADUser -Name Potato$i `
+    New-ADUser -Name AdUser$i `
                -AccountPassword (ConvertTo-SecureString -AsPlainText "London2019*" -Force) `
                -City London `
                -Company QA  `
-               -DisplayName Potato$i `
+               -DisplayName AdUser$i `
                -Enabled $True `
                -PasswordNeverExpires $True `
-               -SamAccountName Potato$i `
-               -UserPrincipalName "Potato$i@$domainname"
+               -SamAccountName AdUser$i `
+               -UserPrincipalName "AdUser$i@$domainname"
     if ($i -lt 10) {
         $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '0-Zeros'"
-        Set-ADUser -Identity "Potato$i" -Department Zeros
-        Get-ADUser -Identity "potato$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName 
+        Set-ADUser -Identity "AdUser$i" -Department Zeros
+        Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName 
     }
     elseif ($i -ge 10 -and $i -lt 20) { 
         $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '1-Tens'"
-        Set-ADUser -Identity "Potato$i" -Department Tens
-        Get-ADUser -Identity "potato$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
+        Set-ADUser -Identity "AdUser$i" -Department Tens
+        Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
     }elseif ($i -ge 20 -and $i -lt 30) { 
         $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '2-Twenties'"
-        Set-ADUser -Identity "Potato$i" -Department Twenties
-        Get-ADUser -Identity "potato$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
+        Set-ADUser -Identity "AdUser$i" -Department Twenties
+        Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
     }elseif ($i -ge 30 -and $i -lt 40) { 
         $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '3-Thirties'"
-        Set-ADUser -Identity "Potato$i" -Department Thirties
-        Get-ADUser -Identity "potato$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
+        Set-ADUser -Identity "AdUser$i" -Department Thirties
+        Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
     }elseif ($i -ge 40 -and $i -lt 50) { 
         $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '4-Forties'"
-        Set-ADUser -Identity "Potato$i" -Department Forties
-        Get-ADUser -Identity "potato$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
+        Set-ADUser -Identity "AdUser$i" -Department Forties
+        Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
     }elseif ($i -ge 50 -and $i -lt 60) { 
         $ADOU=Get-ADOrganizationalUnit -Filter "name -eq '5-Fifties'"
-        Set-ADUser -Identity "Potato$i" -Department Fifties
-        Get-ADUser -Identity "potato$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
+        Set-ADUser -Identity "AdUser$i" -Department Fifties
+        Get-ADUser -Identity "AdUser$i" | Move-ADObject -TargetPath $ADOU.DistinguishedName
     }
-    Add-ADGroupMember -Identity 'los potatos' -Members "potato$i"
+    Add-ADGroupMember -Identity 'los AdUsers' -Members "AdUser$i"
     Add-ADGroupMember -Identity 'Domain Admins' -Members "Domain Users"
 }
 
