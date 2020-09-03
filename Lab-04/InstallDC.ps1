1b9b9d810177241ef1005613ed49f9ee26f546ed
cemvarol <cem.varol@outlook.com>
2020-09-03 15:39:15 +0100

    Update InstallDC.ps1

--- Lab-04/InstallDC.ps1
+++ Lab-04/InstallDC.ps1
@@ -1,1 +1,16 @@
-sadads
+install-windowsfeature AD-Domain-Services -IncludeManagementTools
+Import-Module ADDSDeployment
+$dsrmPassword = (ConvertTo-SecureString -AsPlainText -Force -String "Potato2019")
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "aurian.club" `
+-DomainNetbiosName "AURIAN" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-SafeModeAdministratorPassword $dsrmPassword `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
