2aeb5f46aebfa05b00a83f3dcbf24573e165c237
cemvarol <cem.varol@outlook.com>
2020-09-15 13:19:15 +0100

    Update SetHost.ps1

--- Lab-03/SetHost.ps1
+++ Lab-03/SetHost.ps1
@@ -1,1 +1,32 @@
-aa
+Set-NetFirewallProfile -Enabled False
+
+Install-WindowsFeature Routing, RSAT-RemoteAccess, Hyper-V-Tools, Hyper-V-PowerShell
+
+Start-Sleep -s 3
+
+New-VMSwitch -SwitchName VMs -SwitchType Internal
+
+Start-Sleep -s 3
+
+Netsh int ip set address "vEthernet (VMs)" static 1.1.1.1 255.0.0.0
+
+Start-Sleep -s 3
+
+Install-WindowsFeature Routing -IncludeManagementTools
+Install-RemoteAccess -VpnType Vpn
+
+Start-Sleep -s 3
+
+$ExternalInterface="Ethernet 2"
+$InternalInterface="vEthernet (VMs)"
+
+netsh routing ip nat install
+netsh routing ip nat add interface $ExternalInterface
+netsh routing ip nat set interface $ExternalInterface mode=full
+netsh routing ip nat add interface $InternalInterface
+
+Netsh routing IP NAT add portmapping name= $ExternalInterface tcp 0.0.0.0 80 1.1.1.2 80
+
+Start-Sleep -s 3
+
+Start-Process Powershell.exe -Argumentlist "-file C:\SC\2-VhdsNChrome.ps1"
