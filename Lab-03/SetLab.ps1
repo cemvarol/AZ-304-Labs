12f0106507c7061175380147f214fd513bfbba35
cemvarol <cem.varol@outlook.com>
2020-09-15 13:05:38 +0100

    Update SetLab.ps1

--- Lab-03/SetLab.ps1
+++ Lab-03/SetLab.ps1
@@ -11,15 +11,15 @@ Set-TimeZone "GMT Standard Time"
 cd\
 mkdir SC
 
-$url1 = "https://raw.githubusercontent.com/cemvarol/AZ-303-Labs/master/Lab-09/SetHost.ps1"
+$url1 = "https://github.com/cemvarol/AZ-304-Labs/blob/master/Lab-03/SetHost.ps1"
 $output1 = "C:\SC\1-SetHost.ps1"
 Invoke-WebRequest -Uri $url1 -OutFile $output1
 
-$url2 = "https://raw.githubusercontent.com/cemvarol/AZ-303-Labs/master/Lab-09/Vhd%26Chrome.ps1"
+$url2 = "https://raw.githubusercontent.com/cemvarol/AZ-304-Labs/master/Lab-03/vhd%26chrome.ps1"
 $output2 = "C:\SC\2-VhdsNChrome.ps1"
 Invoke-WebRequest -Uri $url2 -OutFile $output2
 
-$url3 = "https://raw.githubusercontent.com/cemvarol/AZ-303-Labs/master/Lab-09/CreateStartVM.ps1"
+$url3 = "https://github.com/cemvarol/AZ-304-Labs/blob/master/Lab-03/CreateStartVM.ps1"
 $output3 = "C:\SC\3-CreateVMs.ps1"
 Invoke-WebRequest -Uri $url3 -OutFile $output3
 
