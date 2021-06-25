d6ea82825a760dda20a82a369896cbd1bf070a8d
cemvarol <cem.varol@outlook.com>
2021-06-25 11:51:11 +0100

    Create sc.ps1

---
+++ IDsLab/sc.ps1
@@ -0,0 +1,7 @@
+$ssPtr = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($secret.KeyValue)
+try {
+   $secretValueText = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($ssPtr)
+} finally {
+   [System.Runtime.InteropServices.Marshal]::ZeroFreeBSTR($ssPtr)
+}
+Write-Output $secretValueText
